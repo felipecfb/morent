1fb7ace1df77050a43891a88d7620155969b74a2
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-09-20 15:32:05 -0300

    feat: creating button component

---
+++ src/app/components/Button/index.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { CaretDown } from '@phosphor-icons/react'
+
+export type ButtonVariation = 'primary' | 'secondary' | 'minimal'
+
+export type ButtonProps = {
+  disabled?: boolean
+  title?: string
+  rightIcon?: boolean
+  variation?: ButtonVariation
+}
+
+export function Button({
+  disabled,
+  title,
+  rightIcon = false,
+  variation = 'primary',
+  ...rest
+}: ButtonProps) {
+  return (
+    <button
+      className={`px-4 py-1 rounded-md font-semibold flex items-center gap-2 group border-[inset] ${
+        variation === 'primary'
+          ? 'bg-primary-500 text-primary-0 focus:border-2 focus:border-[#CEBEFE] focus:bg-primary-700'
+          : variation === 'secondary'
+          ? 'bg-primary-0 border-2 border-secondary-300 text-secondary-400 hover:border-secondary-400 hover:text-secondary-500 focus:bg-primary-100 focus:text-secondary-500'
+          : 'text-secondary-200 hover:bg-primary-0 hover:text-secondary-500 hover:fill-secondary-500 focus:border-2 focus:border-secondary-200 focus:bg-primary-100 focus:text-secondary-500 disabled:text-secondary-400'
+      }
+      ${disabled && 'opacity-40'}
+      `}
+      {...rest}
+    >
+      {title && title}
+      {rightIcon && (
+        <CaretDown
+          size={12}
+          weight="fill"
+          className={`${
+            variation === 'primary'
+              ? 'fill-primary-0'
+              : variation === 'secondary'
+              ? 'fill-primary-400 group-hover:fill-secondary-500 group-focus:fill-secondary-500'
+              : 'fill-primary-200 group-hover:fill-secondary-500 group-focus:fill-secondary-500'
+          }`}
+        />
+      )}
+    </button>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import { Button } from './components/Button'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="w-full h-screen flex flex-col items-center justify-center gap-4 bg-secondary-700">
+      <div className="flex items-center gap-2">
+        <Button variation="primary" title="Label" rightIcon />
+        <Button variation="primary" title="Label" />
+        <Button variation="primary" title="Label" disabled />
+      </div>
+      <div className="flex items-center gap-2">
+        <Button variation="secondary" title="Label" rightIcon />
+        <Button variation="secondary" title="Label" />
+        <Button variation="secondary" title="Label" disabled />
+      </div>
+      <div className="flex items-center gap-2">
+        <Button variation="minimal" title="Label" rightIcon />
+        <Button variation="minimal" title="Label" />
+        <Button variation="minimal" title="Label" disabled />
+      </div>
     </div>
   )
 }
