42c3ad75b7a7272c27820486d351814dd3f02d0d
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-09-26 11:43:42 -0300

    styles: adding border bottom in header

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { Utilities } from './components/Utilities'
 
 export function Header() {
   return (
-    <header className="w-full bg-primary-0 py-10 pl-14 pr-8 flex items-center gap-16 justify-between">
+    <header className="w-full bg-primary-0 py-10 pl-14 pr-8 flex items-center gap-16 justify-between border-b-[1px] border-[#C3D4E966]">
       <div className="flex items-center gap-16 w-full">
         <h1 className="text-primary-500 font-bold font-primary text-3xl uppercase">
           Morent
