f134e1fa8365a15727126e0e157af5978d0bda66
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-09-26 11:42:02 -0300

    feat: adding item for has notification

--- src/app/components/Header/components/Utilities/Icon.tsx
+++ src/app/components/Header/components/Utilities/Icon.tsx
@@ -11,26 +11,28 @@ export enum IconType {
 interface IconProps {
   type: IconType
   href?: string
+  hasNotification?: boolean
 }
 
-export function Icon({ type, href = '' }: IconProps) {
+export function Icon({ type, href = '', hasNotification = false }: IconProps) {
   return (
-    <div className="w-10 h-10 rounded-full bg-primary-0 flex items-center justify-center  border-[1px] border-[#C3D4E966]">
-      {type === IconType.Like && (
-        <a href={href}>
+    <div
+      className={`w-10 h-10 rounded-full bg-primary-0 flex items-center justify-center border-[1px] border-[#C3D4E966] relative ${
+        hasNotification &&
+        "after:content-[''] after:absolute after:w-2 after:h-2 after:bg-[#FF4423] after:rounded-full after:top-0 after:right-0"
+      }`}
+    >
+      <a href={href}>
+        {type === IconType.Like && (
           <Heart size={24} className="fill-secondary-400" weight="fill" />
-        </a>
-      )}
-      {type === IconType.Notification && (
-        <a href={href}>
+        )}
+        {type === IconType.Notification && (
           <Bell size={24} className="fill-secondary-400" weight="fill" />
-        </a>
-      )}
-      {type === IconType.Settings && (
-        <a href={href}>
+        )}
+        {type === IconType.Settings && (
           <GearSix size={24} className="fill-secondary-400" weight="fill" />
-        </a>
-      )}
+        )}
+      </a>
     </div>
   )
 }
--- src/app/components/Header/components/Utilities/index.tsx
+++ src/app/components/Header/components/Utilities/index.tsx
@@ -7,7 +7,7 @@ export function Utilities() {
   return (
     <div className="flex items-center gap-5">
       <Icon type={IconType.Like} />
-      <Icon type={IconType.Notification} />
+      <Icon type={IconType.Notification} hasNotification />
       <Icon type={IconType.Settings} />
       <Profile />
     </div>
